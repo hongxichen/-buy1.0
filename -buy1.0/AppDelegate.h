080352f93de538dfccc4432b8f1f7fbf654d55b6
hongxichen <hxc@hongxichendemacbook-pro.local>
2016-04-08 15:34:14 +0800

    Initial Commit

---
+++ -buy1.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  -buy1.0
+//
+//  Created by 洪曦尘 on 16/4/8.
+//  Copyright © 2016年 洪曦尘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
